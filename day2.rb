efd255661c27e26669f4e1101ab780056be0ba5f
Andrew Ausley <aausley@squareup.com>
2022-12-03 07:21:05 -0500

    Responding to comments

--- day2.rb
+++ day2.rb
@@ -8,12 +8,10 @@
 # A = Rock, B = Paper, C = Scissors
 
 def calculate_total_score_of_two_throws(file = 'd2.txt')
-  my_score = 0
-  File.read(file).each_line.map do
+  my_score = File.read(file).each_line.sum do
     elf_throw, my_throw = _1.split
-    my_score += paper_rock_scissors_score(elf_throw, my_throw)
+    paper_rock_scissors_score(elf_throw, my_throw)
   end
-
   puts my_score
 end
 
@@ -27,9 +25,7 @@ end
 
 def get_outcome_score(outcome)
   outcome_score = { win: 6, draw: 3, lose: 0 }
-  raise 'Invalid throw!' unless outcome_score.include?(outcome)
-
-  outcome_score[outcome]
+  outcome_score.fetch(outcome)
 end
 
 def get_outcome(elf, me)
@@ -45,18 +41,13 @@ end
 
 def get_throw_score(throw)
   throw_score = { rock: 1, paper: 2, scissors: 3 }
-  raise 'Invalid throw!' unless throw_score.include?(throw)
-
-  throw_score[throw]
+  throw_score.fetch(throw)
 end
 
 def decode_throw(throw)
   throws = { 'X' => :rock, 'Y' => :paper, 'Z' => :scissors, 'A' => :rock, 'B' => :paper, 'C' => :scissors }
-  raise 'Invalid throw!' unless throws.include?(throw)
-
-  throws[throw]
+  throws.fetch(throw)
 end
-
 
 # I'm taking a different approach. I'm letting each throw have a numeric value 0-2 and representing the outcome
 # as the result of Throw1 - Throw2. If I choose the values carefully, I can get the desired throw based on the
@@ -69,17 +60,15 @@ def part2(file = 'd2.txt')
   # Based on the results of My_throw_index minus your_throw_index mod 3
   outcome_value_map = { 'X' => -1, 'Y' => 0, 'Z' => 1 }
 
-  my_score = 0
-  File.read(file).each_line.map do
+  my_score = File.read(file).each_line.sum do
     elf_throw, desired_outcome = _1.split
     elf_index = throw_index_map[elf_throw]
     outcome_value = outcome_value_map[desired_outcome]
     outcome = outcomes[desired_outcome]
     my_throw_index = (elf_index + outcome_value) % 3
     my_throw = throws[my_throw_index]
-    my_score += get_outcome_score(outcome) + get_throw_score(my_throw)
+    get_outcome_score(outcome) + get_throw_score(my_throw)
   end
-
   puts my_score
 end
 
